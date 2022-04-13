bdd2e62fb28427ac25941d8f647b448e0db38992
William <william.jsson@hotmail.com>
2022-04-14 00:39:35 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -20,15 +20,19 @@ def removeWidget(widget):
 
 def mainChatFunction():
     removeWidget(button)
+    removeWidget(label1)
     chatFrame = ttk.LabelFrame(root, text="Main Chatroom", width=height, height=width)
-    # chatFrame.place(x=int(width)/2, y=int(height)/2)
     chatFrame.place(x=0, y=0)
+    chatTextEnter = ttk.Entry(root)
+    chatTextEnter.place(x=20, y=260)
 
 
 button = ttk.Button(root, text="Join Chatroom", style="Accentbutton", command=mainChatFunction)
+label1 = ttk.Label(root, text="Press to join the main chatroom")
 style.configure("Accentbutton", foreground="black")
 
 # Packing
 button.pack()
+label1.pack()
 root.mainloop()
 
